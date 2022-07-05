3f1f6fbd0da91f8128cafe5c6782b9cd56cf038d
Joss <jossappletonfox@protonmail.com>
2022-07-05 22:31:02 +0100

    Be strict with amd64 arch

--- dotfiles/.local/install/install-virtualbox.sh
+++ dotfiles/.local/install/install-virtualbox.sh
@@ -3,19 +3,27 @@
 # Install Oracle VirtualBox
 #
 # Source: https://www.virtualbox.org/wiki/Linux_Downloads
+# Documentation: https://www.oracle.com/technical-resources/articles/it-infrastructure/admin-manage-vbox-cli.html
+
+# Quit if not amd64 arch.
+arch=$(dpkg --print-architecture)
+case $arch in
+	*amd64) : ;;
+	*) echo "unsupported architecture $arch" >&2 ; exit 1 ;;
+esac
 
 # Update the package index
 sudo apt-get update
 
 # Install the dependencies
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install ca-certificates curl gnupg lsb-release linux-headers-amd64
 
 # Add the gpg key to the gpg keyring
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc \
 	| sudo gpg --dearmor -o /usr/share/keyrings/oracle-virtualbox-2016.gpg
 
 # Add the source to /etc/apt/sources.list.d/virtualbox.list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" \
+echo "deb [arch=$arch signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" \
 	| sudo tee /etc/apt/sources.list.d/virtualbox.list >/dev/null
 
 # The key fingerprint for oracle_vbox_2016.asc is
